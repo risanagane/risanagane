f4987abbfc773f34782377dd286c4425f5ed8c63
risanagane <naganegi2673@gmail.com>
2019-10-18 18:42:24 +0900

    課題提出 PHP - 変数と定数

--- taskCalc.php
+++ taskCalc.php
@@ -21,7 +21,29 @@
 
  <p><?php echo('サイコロを2つ降ったときに、総計が8となる確率は？？= '. round(5/36*100,2) .'%'); ?></p>
 
-</body>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskCalc</title>
+</head>
+<body>
+    <p><?php echo('回答を<p>タグで囲み、回答を出力してください。'); ?></p>
+    <p><?php echo('回答例: 1 + 1 = ' . (1 + 1) . 'です。'); ?></p>
+
+    <p><?php echo(' ( 1 + 3 ) * 5 = ' . ( 1 + 3 ) *5); ?></p>
+    <p><?php echo(' ( 22 / 3 )   = ' . round ( 22 / 3 , 2 ) ); ?></p>
+    <p><?php echo(' ( 22 / 3 )   = ' . ceil ( 22 / 3  ) ); ?></p>
+    <p><?php echo(' ( 22 / 3 )   = ' . floor ( 22 / 3  ) ); ?></p>
+    <p><?php echo( round ( 5 / 36 *100 , 2 ) .'%' ); ?></p>
+
+
+    </body>
 </html>
-
-
