1448f4cf908b36ab0b3c012a1070f70b0ede5463
risanagane <naganegi2673@gmail.com>
2019-12-16 20:09:10 +0900

    課題提出　PHP　－データ挿入

---
+++ db/insert.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskExternal</title>
+</head>
+<body>
+<?php
+// country
+echo $sql = "SELECT * FROM `country`";
+
+// heritage
+echo $sql = "SELECT * FROM `heritage`";
+?>
+
+</body>
+</html>
