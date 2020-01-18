b4de84806b3ab1bd7997a8e08150f35cc5b31211
risanagane <naganegi2673@gmail.com>
2020-01-18 17:26:50 +0900

    PHP　課題提出ーデータ削除

---
+++ db/delete.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskExternal</title>
+</head>
+<body>
+<?php
+// 小笠原諸島、姫路城削除
+echo $sql = "delete from heritage where id = 2 and 3";
+?>
+</body>
+</html>
